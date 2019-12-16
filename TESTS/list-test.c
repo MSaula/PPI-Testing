23605b37103bc90a5e6bf4a7193ba4e65d7bb99f
miquelsaula <miquelsaula@gmail.com>
2019-12-16 11:59:00 +0100

    Test case de delete

--- TESTS/list-test.c
+++ TESTS/list-test.c
@@ -28,3 +28,32 @@ TEST_CASE("Comprovant el insert", "[INSERT]")
 {
 
 }
+
+TEST_CASE("Comprovant l'esborrat d'una persona", "[DELETE]")
+{
+    List l;
+    Person p1, p2;
+
+    l = LIST_create();
+
+    p1 = PERSON_create("Ramon", 93);
+    p2 = PERSON_create("Antonia", 345);
+
+    LIST_add(&l, p1);
+    LIST_add(&l, p2);
+
+    SECTION("Comprovant l'eliminació d'una persona inexistent") {
+        REQUIRE(LIST_deletePerson(&l, "Miquel") == -1);
+    }
+
+    SECTION("Comprovant l'eliminació d'una persona existent") {
+        REQUIRE(LIST_deletePerson(&l, "Ramon") == 1);
+    }
+
+    SECTION("Comprovant la eliminació completa") {
+        LIST_deletePerson(&l, "Ramon");
+        LIST_deletePerson(&l, "Antonia");
+        REQUIRE(LIST_isEmpty(l));
+    }
+
+}
